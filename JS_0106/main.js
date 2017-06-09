ba4e71b09cccb3676a707423b6925c8c4ed6f588
antonina88 <rozhenko_antonia@mail.ru>
2017-06-09 12:04:40 +0300

    some update in reg expression

--- JS_0106/main.js
+++ JS_0106/main.js
@@ -1,3 +1,4 @@
+//Задание 1
 let str = "2016/05/20-12:00:35+0300";
 
 let regDate = /(\d{4}(?=\/))\/(\d{2})\/(\d{2})/g;
@@ -14,33 +15,25 @@ console.log(objDate.getTime());
 
 //Задание 2
 let str2 = "apple:2016/5/27__bid_203.38-ask_203.43|2016/5/28__bid_203.35-ask_203.42|2016/5/28__bid_203.39-ask_203.45";
-
-let reDate = /\d{4}\/\d{1,2}\/\d{1,2}/g;
-let reBid = /\d{3}.\d{2}(?=-ask)/g
-let reAsk = /(\d{3,}\.\d{2,})(?!-ask)/g;
-let reBrand = /^[a-z]+/g;
-
-let dateArr = reDate.exec(str2);
-let bid = reBid.exec(str2);
-let ask = reAsk.exec(str2);
-let brandName = reBrand.exec(str2);
+let strArr = str2.split(":");
+let re = /(\d{4}\/\d{1,2}\/\d{1,2})__bid_(\d{1,}.?\d{1,})-ask_(\d{1,}.?\d{1,})/g;
 
 function Brand(brandName) {
 	this.stockName = brandName,
 	this.rates = []
 } 
-Brand.prototype.addRate = function(data, bid, ask) {
+Brand.prototype.addRate = function(reg, str) {
 	let newRate = {};
-	for (let i = 0; i < ask.length; i++) {
+	while ((data = re.exec(str)) !== null)  {
 		newRate = {
-			data: data[i],
-			bid: bid[i],
-			ask: ask[i]
+			data: data[1],
+			bid: data[2],
+			ask: data[3]
 		} 
 		this.rates.push(newRate);	
 	}
 }
-let firstBrand = new Brand(brandName[0]);
-firstBrand.addRate(dateArr,bid,ask);
+let firstBrand = new Brand(strArr[0]);
+firstBrand.addRate(re, strArr[1]);
+
 console.log(firstBrand);
-console.log(dateArr);
